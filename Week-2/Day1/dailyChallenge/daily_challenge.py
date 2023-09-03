50378e6c4140b83ccc7d89ecef20268469c6980c
eddlevy <elevypo@gmail.com>
2023-09-03 16:43:31 +0300

    daily challenge week2-day1

---
+++ Week-2/Day1/dailyChallenge/daily_challenge.py
@@ -0,0 +1,22 @@
+# daily challenge
+# 1
+user_string = input("Write a string \n")
+
+if len(user_string) < 10:
+    print("string not too long")
+
+elif len(user_string) > 10:
+    print("string too long")
+
+else:
+    print("perfect string")
+
+# 2
+
+
+print(user_string[0])
+print(user_string[len(user_string)-1])
+
+# 3
+for i in user_string:
+    print(i)
