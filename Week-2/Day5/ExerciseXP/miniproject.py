bfe6feca12912c4715f7f53f33ba94a1c7758b52
eddlevy <elevypo@gmail.com>
2023-09-07 23:48:04 +0300

    game commitv 1

---
+++ Week-2/Day5/ExerciseXP/miniproject.py
@@ -0,0 +1,9 @@
+
+main_board = ["" for x in range(9)]
+
+def print_main_board():
+  row_1 = f"|{main_board[0]}|{main_board[1]}|{main_board[2]}"
+  row_2 = f"|{main_board[0]}|{main_board[1]}|{main_board[2]}"
+  row_3 = f"|{main_board[0]}|{main_board[1]}|{main_board[2]}"
+
+  print("")
