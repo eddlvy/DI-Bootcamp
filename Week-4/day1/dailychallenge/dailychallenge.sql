7d6fa78d6d7d60c5988de328b75d450cf787178b
eddlevy <elevypo@gmail.com>
2023-09-18 16:15:45 +0300

    week4 day1 daily challenge

---
+++ Week-4/day1/dailychallenge/dailychallenge.sql
@@ -0,0 +1,21 @@
+-- create table actors (
+-- actor_id serial primary key,
+-- first_name varchar(50) not null,
+-- last_name varchar(50) not null,
+-- age date,
+-- number_oscars smallint not null
+-- )
+-- ;
+-- insert into actors (first_name,last_name,age,number_oscars)
+-- values
+-- ('Matt','Damon','1970-10-08',5),
+-- ('George','Clooney','1961-05-06',2)
+-- ;
+-- select count(actor_id) as number_of_actors from actors;
+-- insert into actors (first_name,last_name,age,number_oscars)
+-- values ('Arnold',null,null,5);
+-- select * from actors;
+
+
+
+
